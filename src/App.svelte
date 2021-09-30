a94a57d9992e3fae91f54065803f4d11f3349c17
cn3085 <cn3085@gmail.com>
2021-09-30 13:29:31 +0900

    resolve

--- src/App.svelte
+++ src/App.svelte
@@ -4,14 +4,7 @@
   import routes from "./js/routes";
   import Header from "./components/Header.svelte";
   import Nav from "./components/Nav.svelte";
-<<<<<<< HEAD
   import Toasts from "./components/Toasts.svelte";
-=======
-  import Login from "./pages/login/Login.svelte";
-  import {isValidLogin} from './js/LoginService'
-
-  let isLoginUser = isValidLogin();
->>>>>>> 4c0ab03282e0725c0a26cebf9e71cca4a9157818
 
   function conditionsFailed(event) {
     console.error("conditionsFailed event", event.detail);

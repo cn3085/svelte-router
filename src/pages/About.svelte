0951c3c41aad21977f511df03970331abbe36e74
cn3085 <cn3085@gmail.com>
2021-10-18 17:03:08 +0900

    auto complete

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,10 +1,66 @@
 <script>
-  import { alertError, alertInfo, alertSuccess } from "../js/toast_store";
-  import Toast from '../components/Toast.svelte'
+  import AutoComplete from 'simple-svelte-autocomplete';
+  import DotBlue from "../components/member/DotBlue.svelte";
+  import DotRed from "../components/member/DotRed.svelte";
+  import  axios from "axios";
+
+  let searchKeyword = '';
+  $: selecedVersion = [];
+  $: {
+    selecedVersion;
+    if(document.querySelector('#versionSearchInput') !== null){
+      document.querySelector('#versionSearchInput').value = '';
+    }
+  }
+
+
+  async function searchFunction(){
+    const res = await axios.get('https://hdolt.tovair.com/stadmin/system/route/list?brn_code=SOD');
+    return res.data.data;
+  }
+
+  function valueFunction(value){
+    if(value !== ''){
+      selecedVersion = [...selecedVersion, value];
+      searchKeyword = '';
+    }
+    console.log(searchKeyword, selecedVersion);
+  }
+  
+  function clearInput(value){
+    if(value){
+      
+    }
+  }
+
 </script>
 
-<div>About</div>
-<button on:click={() => alertSuccess(990000, "성공.")}>성공</button>
-<button on:click={() => alertInfo(990000, "경고", "이미 예약된 회원입니다.\n최왈왈: 오락실")}>경고</button>
-<button on:click={() => alertError(990000, "실패", "다시 시도하세요")}>에러</button>
-<Toast/>
+
+<AutoComplete
+  inputId="versionSearchInput"
+  bind:selectedItem={searchKeyword}
+  searchFunction={searchFunction}
+  valueFunction={valueFunction}
+  labelFieldName="version"
+  delay=200
+  localFiltering=false
+  noResultsText="검색 결과가 없습니다."
+  onChange={clearInput}>
+  
+  <div slot="item" let:item={item} let:label={label}>
+    {@html label}
+    {#if item.use === 'Y'}
+      <DotRed/>
+      {:else}
+      <DotBlue/>
+      {/if}
+      <span>{item.use}</span>
+  </div>
+
+</AutoComplete>
+
+  <div>
+    {#each selecedVersion as v}
+      <div>{v.version}</div>
+    {/each}
+  </div>

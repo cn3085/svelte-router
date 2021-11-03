4ddbff7cbcef34cedbfb9d9c57810f24c09675c0
cn3085 <cn3085@gmail.com>
2021-11-03 16:47:35 +0900

    number count

---
+++ src/components/common/NumberCounter.svelte
@@ -0,0 +1,29 @@
+<script>
+	import {onMount, onDestroy, afterUpdate} from 'svelte';
+	
+	export let purpose = 0;
+    export let speed = 1000;
+	let intervalId;
+	$ : i = 0;
+	
+	function countUp(){
+        i++;
+	}
+	
+	onMount( async () => {
+		intervalId = setInterval(() => countUp(), speed);
+	})
+	
+	onDestroy( async () => {
+		clearInterval(intervalId);
+	})
+	
+	afterUpdate( async () => {
+		if(i >= purpose){
+			clearInterval(intervalId);	
+		}
+	})
+</script>
+
+
+{j}
